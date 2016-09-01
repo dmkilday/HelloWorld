fb2d82be59201f4813a6b8d6eeb61b888d1d1a8a
dmkilday <BeautifulJesu5>
2016-08-31 23:35:54 -0600

    Added console logic.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,10 +3,23 @@ package com.example.helloworld;
 /**
  * Created by Damian on 8/28/2016.
  */
+
+import java.io.Console;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
-        printLine("Hello, World!");
+
+        Console c = System.console();
+
+        if (c == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+
+        String name = c.readLine("Enter your name: ");
+
+        printLine("Hello, " + name + "!");
         System.out.println("Your request has been printed.");
     }
 
